d0d47c2f6399eb0116a7ff91918621a3f0f4b10e
boardguy1024 <boardguy1024@gmail.com>
2021-09-28 21:29:25 +0900

    Initial Commit

---
+++ Shared/SwiftUI_ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatAppApp.swift
+//  Shared
+//
+//  Created by park kyung seok on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
